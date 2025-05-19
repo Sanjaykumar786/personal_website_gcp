fd6b08bd8059906e47b0bbafe3bbad2e12e92d8f
unknown <sanjaydhaka47@gmail.com>
2025-05-19 19:03:58 -0400

    added all files to git

---
+++ main.tf
@@ -0,0 +1,42 @@
+provider "google" {
+  credentials = file("mywebpage-460319-031b57bb08ac.json")
+  project     = "mywebpage-460319"
+  region      = var.region
+}
+
+resource "google_storage_bucket" "static_site" {
+  name          = "mywebpage-website"
+  location      = var.region
+  force_destroy = true
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "index.html"
+  }
+}
+
+resource "google_storage_bucket_object" "html" {
+  name   = "index.html"
+  bucket = google_storage_bucket.static_site.name
+  source = "index.html"
+  content_type = "text/html"
+}
+
+resource "google_storage_bucket_object" "image" {
+  name   = "sanjaykumar.jpeg"               # Path in the bucket
+  bucket = google_storage_bucket.static_site.name
+  source = "sanjaykumar.jpeg"  # Local path to your image file
+  content_type = "image/jpeg"
+}
+
+resource "google_storage_bucket_object" "css" {
+  name   = "style.css"
+  bucket = google_storage_bucket.static_site.name
+  source = "style.css"
+  content_type = "text/css"
+}
+
+resource "google_storage_bucket_iam_binding" "public_rule" {
+  bucket = google_storage_bucket.static_site.name
+  role   = "roles/storage.objectViewer"
+  members = ["allUsers"]
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "project_id" {
+  description = "your project_id"
+  default = "mywebpage-460319"
+  type        = string
+}
+
+variable "region" {
+  description = "Region to deploy in"
+  type        = string
+  default     = "US"
+}
